5db4ee319fc0c7310cb7ba95ac1566ec172eb03d
LeDucTam2041992 <you@example.com>
2020-07-03 17:01:28 +0700

    up

---
+++ src/baitap3/baitap3.java
@@ -0,0 +1,14 @@
+package baitap3;
+
+import java.util.Scanner;
+
+public class baitap3 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int rate = 23000;
+        System.out.print("Enter your dollars : ");
+        float uSD = scanner.nextFloat();
+        float vND = rate*uSD;
+        System.out.printf("Your VND = %10.0f vnd",vND);
+    }
+}
